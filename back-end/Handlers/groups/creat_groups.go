28b0e4bd5f2e0ccc04553ff5e3b2ed13ffc56212
Yossf-jaouhar <jwhryosf@gmail.com>
2025-04-29 13:46:13 +0100

    creatgroups are done

--- back-end/Handlers/groups/creat_groups.go
+++ back-end/Handlers/groups/creat_groups.go
@@ -2,10 +2,10 @@ package groups
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
+	"socialN/Handlers/auth"
 	dataB "socialN/dataBase"
 )
 
@@ -14,29 +14,36 @@ type Group struct {
 	Description string `json:"description"`
 }
 
+
+// Insert the groups in the database....
 func Creat_Groups(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("kdhlt")
-	// userID, err := auth.ValidateSession(r, dataB.SocialDB)
-	// if err != nil {
-	// 	http.Error(w, "Invalid session", http.StatusUnauthorized)
-	// 	return
-	// }
+	userID, err := auth.ValidateSession(r, dataB.SocialDB)
+	if err != nil {
+		http.Error(w, "Invalid session", http.StatusUnauthorized)
+		return
+	}
 
 	var group Group
 
-	err := json.NewDecoder(r.Body).Decode(&group)
+	err = json.NewDecoder(r.Body).Decode(&group)
 	if err != nil {
-		 http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
-	
+
+	if group.Title == "" || group.Description == "" {
+		http.Error(w, "Invalid JSONm Title and Description are required...", http.StatusBadRequest)
+		return
+	}
+
 	query := `
 	  INSERT INTO groups (title , description , adminId) VALUES (?,?,?);
 	`
 
-	_, err = dataB.SocialDB.Exec(query, group.Title, group.Description, 1)
+	_, err = dataB.SocialDB.Exec(query, group.Title, group.Description, userID)
 	if err != nil {
 		log.Println("Error to insert groups in db:", err)
+		http.Error(w, "Failed to create group. Please try again later.", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
