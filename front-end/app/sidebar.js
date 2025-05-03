e62348cb60ef7a81ab47298697eb921428971c54
Bilal Daanouni <daanouni00bilal@gmail.com>
2025-05-03 16:10:19 +0100

    adding hide menu

--- front-end/app/sidebar.js
+++ front-end/app/sidebar.js
@@ -1,35 +1,46 @@
 'use client'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import './globals.css';
 
 export default function SideBar() {
     const pathname = usePathname();
-    console.log(pathname);
+    const [collapsed, setCollapsed] = useState(false);
 
+    const toggleSidebar = () => {
+        setCollapsed(!collapsed);
+    };
 
     return (
-        <aside className="sidebar">
-            <img className='logo' src='../images/logo.svg'></img>
+        <aside className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
+            <div className='logoAndHide'>
+                <img className='logo' src='/images/logo.svg' alt='Logo' />
+                <img
+                    src='/images/hideMenu.svg'
+                    alt='Hide menu'
+                    onClick={toggleSidebar}
+                    className={`hideIcon ${collapsed ? 'rotated' : ''}`}
+                />
+            </div>
             <nav className="navBar">
                 <Link href="/home" className={`navLink ${pathname === '/home' ? 'active' : ''}`}>
-                    <img src='../images/home-icon.svg'></img>
-                    Home
+                    <img src='/images/home-icon.svg' alt='Home icon' />
+                    {!collapsed && 'Home'}
                 </Link>
                 <Link href="/groups" className={`navLink ${pathname === '/groups' ? 'active' : ''}`}>
-                    <img src='../images/groups-icon.svg'></img>
-                    Groups
+                    <img src='/images/groups-icon.svg' alt='Groups icon' />
+                    {!collapsed && 'Groups'}
                 </Link>
-                <Link href="/chat" className={`navLink ${pathname === "/chat" ? 'active' : ''}`}>
-                    <img src='../images/chat-icon.svg'></img>
-                    Chat
+                <Link href="/chat" className={`navLink ${pathname === '/chat' ? 'active' : ''}`}>
+                    <img src='/images/chat-icon.svg' alt='Chat icon' />
+                    {!collapsed && 'Chat'}
                 </Link>
                 <Link href="/users" className={`navLink ${pathname === '/users' ? 'active' : ''}`}>
-                    <img src='../images/users-icon.svg'></img>
-                    Users
+                    <img src='/images/users-icon.svg' alt='Users icon' />
+                    {!collapsed && 'Users'}
                 </Link>
             </nav>
         </aside>
     )
-
 }
