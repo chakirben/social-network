85566fc4b64d9c76c31eb89e11a516b4cf15c001
Yossf-jaouhar <jwhryosf@gmail.com>
2025-05-07 12:56:48 +0100

    working in ui groups

--- front-end/app/groups/page.jsx
+++ front-end/app/groups/page.jsx
@@ -1,14 +1,18 @@
+"use client"
 import Groupbar from "@/components/groupbar"
-import MyGroupsPage from "./groups"
+import MyGroupsPage from "./api_and_funcs/get_groups"
 import SideBar from "@/components/sidebar";
-import "./groups.css"
+import "./css/groups1.css"
 
 export default function JustMyGroupsPage() {
+    const handleBTNCreateGroup = () => {
+      DataToCreatGroup()
+    }
     return (
         <div className="home">
             <SideBar />
             <div className="divallGroups">
-              <Groupbar />
+            <Groupbar onCreateGroup={handleBTNCreateGroup} />
               <div className="groupsmn">
                 <MyGroupsPage />
               </div>
--- front-end/components/groupbar.jsx
+++ front-end/components/groupbar.jsx
@@ -1,8 +1,15 @@
-export default function Groupbar() {
+"use client"
+export default function Groupbar({onCreateGroup}) {
     return (
         <div className="groupbar">
-                   <img src="./images/arrow-left.svg" ></img>
-                   <p>Groups</p>
+            <div className="leftgroupbar">
+                <img src="./images/arrow-left.svg" alt="Back" />
+                <p>Groups</p>
+            </div>
+            <div id="btcreateGP" className="rightgroupbar" onClick={onCreateGroup}>
+                <img className="imgplus" src="./images/plus.svg" alt="Add" />
+                <button className="btcreategroup">Create Group</button>
+            </div>
         </div>
     )
 }
