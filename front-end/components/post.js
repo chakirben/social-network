25834c77be8f35cf69558576668e210ea4493e6a
Bilal Daanouni <daanouni00bilal@gmail.com>
2025-05-13 13:15:09 +0100

    adding a modal to the post images

--- front-end/components/post.js
+++ front-end/components/post.js
@@ -1,27 +1,60 @@
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-// import Reaction from './reaction';
 import ReactionGroup from './reactionGroup';
 import { timePassed } from '@/public/utils/timePassed';
 
 export default function Post({ pst }) {
     const router = useRouter();
-    return (
-        <div className="post" onClick={() => router.push(`/post/${pst.id}`)}>
-            <div className="content df cl gp12">
-                <div className="userData">
-                    {pst?.avatar ? <img className="pic sm" src={`http://localhost:8080/${pst?.avatar}`} ></img> : ""}
-                    <h4>{pst?.creator}</h4>
-                    <h5>{"• " +timePassed(pst?.created_at)}</h5>
-                </div>
-                <div className="content">{pst?.content}</div>
-                {pst.image && <img className="pic nrml" src={`http://localhost:8080/${pst?.image}`}></img>}
-            </div>
-            {/* <div className='reactionsContainer df gp12'>
-                <Reaction count={pst.like_count} itemType="post" itemId={pst.id} userReaction={pst.user_reaction} reactionType={"like"} />
-                <Reaction count={pst.dislike_count} itemType="post" itemId={pst.id} userReaction={pst.user_reaction} reactionType={"dislike"} />
-            </div> */}
-            <ReactionGroup className="reactionsContainer df gp12" likeCount={pst.like_count} dislikeCount={pst.dislike_count} itemType="post" itemId={pst.id} userReaction={pst.user_reaction} ></ReactionGroup>
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalImageSrc, setModalImageSrc] = useState('');
 
-        </div>
-    )
+    const openImageModal = (src) => {
+        setModalImageSrc(src);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setModalImageSrc('');
+    };
+
+    return (
+        <>
+            <div className="post" onClick={() => router.push(`/post/${pst.id}`)}>
+                <div className="content df cl gp12">
+                    <div className="userData">
+                        {pst?.avatar ? <img className="pic sm" src={`http://localhost:8080/${pst?.avatar}`} /> : ""}
+                        <h4>{pst?.creator}</h4>
+                        <h5>{"• " + timePassed(pst?.created_at)}</h5>
+                    </div>
+                    <div className="content">{pst?.content}</div>
+
+                    {pst.image && (
+                        <img
+                            className="pic nrml"
+                            src={`http://localhost:8080/${pst?.image}`}
+                            onClick={(e) => {
+                                e.stopPropagation(); // prevent post click navigation
+                                openImageModal(`http://localhost:8080/${pst?.image}`);
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    )}
+                </div>
+                <ReactionGroup
+                    className="reactionsContainer df gp12"
+                    likeCount={pst.like_count}
+                    dislikeCount={pst.dislike_count}
+                    itemType="post"
+                    itemId={pst.id}
+                    userReaction={pst.user_reaction}/>
+            </div>
+
+            {modalOpen && (
+                <div className="image-modal" onClick={closeModal}>
+                    <img src={modalImageSrc} className="modal-img" />
+                </div>
+            )}
+        </>
+    );
 }
