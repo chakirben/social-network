8081150f07ec57049acbe3368ada0e681d41ecb1
Bilal Daanouni <daanouni00bilal@gmail.com>
2025-05-13 12:06:30 +0100

    adding followers and following in profile

--- back-end/Handlers/users/getCurrentUserData.go
+++ back-end/Handlers/users/getCurrentUserData.go
@@ -11,15 +11,17 @@ import (
 )
 
 type user struct {
-	ID          int     `json:"id"`
-	Nickname    *string `json:"nickname"`
-	Email       string  `json:"email"`
-	FirstName   string  `json:"firstName"`
-	LastName    string  `json:"lastName"`
-	DateOfBirth string  `json:"dateOfBirth"`
-	Avatar      *string `json:"avatar"`
-	About       *string `json:"about"`
-	AccountType *string `json:"accountType"`
+	ID             int     `json:"id"`
+	Nickname       *string `json:"nickname"`
+	Email          string  `json:"email"`
+	FirstName      string  `json:"firstName"`
+	LastName       string  `json:"lastName"`
+	DateOfBirth    string  `json:"dateOfBirth"`
+	Avatar         *string `json:"avatar"`
+	About          *string `json:"about"`
+	AccountType    *string `json:"accountType"`
+	FollowersCount *int    `json:"followers"`
+	FollowingCount *int    `json:"following"`
 }
 
 func GetCurrentUserData(w http.ResponseWriter, r *http.Request) {
@@ -31,11 +33,12 @@ func GetCurrentUserData(w http.ResponseWriter, r *http.Request) {
 
 	var u user
 	err = dataB.SocialDB.QueryRow(
-		`SELECT id, nickname, email, firstName, lastName, dateOfBirth, avatar, about, accountType
-		FROM Users WHERE id = ?`, userID).Scan(
+		`SELECT id, nickname, email, firstName, lastName, dateOfBirth, avatar, about, accountType,
+		(SELECT COUNT(*) FROM Followers WHERE followerId =?) AS followersCount,
+		(SELECT COUNT(*) FROM Followers WHERE followedId =?) AS followingCount
+		FROM Users WHERE id = ?`, userID, userID, userID).Scan(
 		&u.ID, &u.Nickname, &u.Email, &u.FirstName, &u.LastName,
-		&u.DateOfBirth, &u.Avatar, &u.About, &u.AccountType,
-	)
+		&u.DateOfBirth, &u.Avatar, &u.About, &u.AccountType, &u.FollowersCount, &u.FollowingCount)
 
 	if err == sql.ErrNoRows {
 		http.Error(w, "User not found", http.StatusNotFound)
