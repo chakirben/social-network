7d968db388b00e1913bac6899828bbb2445eb268
Yossf-jaouhar <jwhryosf@gmail.com>
2025-05-21 12:14:30 +0100

    fix the posts in the groups

--- front-end/app/groups/api_and_funcs/fetch_groups_posts.jsx
+++ front-end/app/groups/api_and_funcs/fetch_groups_posts.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from "react";
+import { useEffect, useState , useRef} from "react";
 import Post from "@/components/post";
 import CreatPostInGroup from "@/components/groups/creat_postgroup";
 import "../../../styles/global.css";
@@ -14,6 +14,13 @@ export default function GroupDetails({ groupId, title }) {
     const [isLoading, setIsLoading] = useState(true);
     const [events, setEvents] = useState([])
     const [activeTab, setActiveTab] = useState("posts");
+
+    const [text, setText] = useState('');
+    const [imageSrc, setImageSrc] = useState(null);
+    const inputRef = useRef(null);
+
+    
+
     useEffect(() => {
         console.log("Updated events:", events);
     }, [events]);
@@ -60,6 +67,63 @@ export default function GroupDetails({ groupId, title }) {
         }
     }, [groupId, activeTab]);
 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const file = inputRef.current.files[0];
+        const formData = new FormData();
+        formData.append('content', text);
+        formData.append('privacy', "inGroup");
+        formData.append('groupId', groupId)
+        if (file) {
+            formData.append('image', file);
+        }
+
+        if (text.trim() == "") {
+            return
+        } else {
+
+            try {
+                const res = await fetch('http://localhost:8080/api/CreatePost', {
+                    method: 'POST',
+                    body: formData,
+                    credentials: 'include',
+
+                });
+                const result = await res.json();
+                setPostsGroup((pv) => [result, ...pv])
+
+                setText('');
+                setImageSrc(null);
+                inputRef.current.value = null;
+            } catch (err) {
+                console.error('Post failed:', err);
+            }
+        }
+    };
+
+
+    const handleImageClick = () => {
+        inputRef.current.click();
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setImageSrc(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    if (isLoading) {
+        return <div>Loading posts of the group...</div>;
+    }
+
+
+
     return (
         <div className='postEventsInGroup'>
             <div className="GPTitle">
@@ -92,7 +156,41 @@ export default function GroupDetails({ groupId, title }) {
 
             {!isLoading && activeTab === "posts" && (
                 <>
-                    <CreatPostInGroup gpid={groupId} />
+                    <form className="creatPostForm" onSubmit={handleSubmit}>
+                        <div className="searchBar">
+                            <img src="./../../../public/images/user-icon.png" />
+                            <input
+                                className="searchInput"
+                                placeholder="What’s happening ?"
+                                value={text}
+                                onChange={(e) => setText(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="ImagePreviewBox">
+                            {imageSrc && <img src={imageSrc} alt="Preview" className="preview-img" />}
+                        </div>
+
+                        <Divider />
+
+                        <div className='spB'>
+                            <div className='group'>
+                                <img
+                                    src="./images/image.svg"
+                                    className="upload-icon"
+                                    onClick={handleImageClick}
+                                />
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    ref={inputRef}
+                                    onChange={handleFileChange}
+                                    style={{ display: 'none' }}
+                                />
+                            </div>
+                            <button type='submit'>post</button>
+                        </div>
+                    </form>
                     {PostsGroup.map((pst) => (
                         <Post key={pst.id} pst={pst} />
                     ))}
