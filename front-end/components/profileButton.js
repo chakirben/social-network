d2dc7a606f6590e7fbafff79bc6858e23e3391ea
bdaanoun <daanouni00bilal@gmail.com>
2025-05-28 17:29:09 +0100

    fixing some issues with notifications

--- front-end/components/profileButton.js
+++ front-end/components/profileButton.js
@@ -7,11 +7,29 @@ export default function ProfileButton() {
     return (
         <div className="profileButton">
             {user ? (
-                <div className="profileInfo df gp12 center" onClick={() => { router.push("/profile") }}>
-                    <img className="avatar" src={"http://localhost:8080/" + user.avatar} />
-                    <div className="profileDetails df col gp6">
-                        <h4>{user.firstName}</h4>
-                        <p>{user.email}</p>
+                <div className="profileInfo df gp12 center" >
+                    <div
+                        className="Notifications" onClick={(e) => {
+                            e.stopPropagation()
+                            router.push("/notifications")
+                        }}
+                    ><img src="./images/notifications.svg"></img>
+                    </div>
+
+
+                    <div
+                        className="avatarAndEmail"
+                        onClick={() => router.push("/profile")}>
+                            
+                        <img
+                            className="avatar"
+                            src={`http://localhost:8080/${user.avatar}`}
+                            alt="Avatar"
+                        />
+                        <div className="profileDetails df col gp6">
+                            <h4>{user.firstName}</h4>
+                            <p>{user.email}</p>
+                        </div>
                     </div>
                 </div>
             )
