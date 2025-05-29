d842b751a7668248a095d6cae52aaa40813a9dad
Yossf-jaouhar <jwhryosf@gmail.com>
2025-05-29 13:23:22 +0100

    worings ui groups

--- front-end/app/groups/api_and_funcs/get_groups.jsx
+++ front-end/app/groups/api_and_funcs/get_groups.jsx
@@ -1,20 +1,44 @@
 "use client";
 import MyGroup from "@/components/groups/mygroups";
 import NoMyGroup from "@/components/groups/notmygroups";
+import { useState } from "react";
 
 export default function MyGroupsPage({ myGroups, notMyGroups, onJoin }) {
   if (myGroups.length === 0 && notMyGroups.length === 0) {
     return <div>No groups found...</div>;
   }
 
+  const [ActiveTab ,  setActiveTab] = useState("mygroups")
+
   return (
+    <>
+    <div className='filterGroups'>
+ 
+             <span className="MyGroupsSpam" 
+              onClick={() => setActiveTab("mygroups")}
+             >
+                    MyGroups
+                </span>
+                <span className="GroupsNotJoinYetSpam">
+                  NotJoinYet
+                </span>
+      
+    </div>
     <div className="groupsmn">
-      {myGroups.map((group) => (
-        <MyGroup key={group.Id} group={group} />
-      ))}
+      {ActiveTab === "mygroups" && (
+        <>
+          {myGroups.map((group) => (
+             <MyGroup key={group.Id} group={group} />
+          ))}
+        </>
+
+      ) 
+      }
+      
       {notMyGroups.map((group) => (
         <NoMyGroup key={group.Id} group={group} onJoin={onJoin} />
       ))}
     </div>
+    </>
   );
 }
