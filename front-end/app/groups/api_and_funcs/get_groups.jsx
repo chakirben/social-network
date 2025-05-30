d6088afebfbd721c022e0bb9a2f4d7eddccbc668
Yossf-jaouhar <jwhryosf@gmail.com>
2025-05-30 17:46:25 +0100

    style groups

--- front-end/app/groups/api_and_funcs/get_groups.jsx
+++ front-end/app/groups/api_and_funcs/get_groups.jsx
@@ -8,37 +8,45 @@ export default function MyGroupsPage({ myGroups, notMyGroups, onJoin }) {
     return <div>No groups found...</div>;
   }
 
-  const [ActiveTab ,  setActiveTab] = useState("mygroups")
+  const [ActiveTab, setActiveTab] = useState("mygroups")
+  console.log("hiiiiiiiiiiiiiiiiggfgfg", ActiveTab);
+
+
+  const handler = (arg) => {
+    setActiveTab(arg)
+  }
 
   return (
     <>
-    <div className='filterGroups'>
- 
-             <span className="MyGroupsSpam" 
-              onClick={() => setActiveTab("mygroups")}
-             >
-                    MyGroups
-                </span>
-                <span className="GroupsNotJoinYetSpam">
-                  NotJoinYet
-                </span>
-      
-    </div>
-    <div className="groupsmn">
-      {ActiveTab === "mygroups" && (
-        <>
-          {myGroups.map((group) => (
-             <MyGroup key={group.Id} group={group} />
-          ))}
-        </>
+      <div className='filterGroups'>
 
-      ) 
-      }
-      
-      {notMyGroups.map((group) => (
-        <NoMyGroup key={group.Id} group={group} onJoin={onJoin} />
-      ))}
-    </div>
+        <span className="MyGroupsSpam" onClick={() => handler("mygroups")}>
+          MyGroups
+        </span>
+        <span className="GroupsNotJoinYetSpam" onClick={() =>  handler("NotJoinYet")}>
+          NotJoinYet
+        </span>
+
+      </div>
+
+      <div className="groupsmn">
+        {ActiveTab === "mygroups" ? (
+          <>
+            {myGroups.map((group) => (
+              <MyGroup key={group.Id} group={group} />
+            ))}
+          </>
+
+        ) : (
+          <>
+            {notMyGroups.map((group) => (
+              <NoMyGroup key={group.Id} group={group} onJoin={onJoin} />
+            ))}
+          </>
+        )}
+
+
+      </div>
     </>
   );
 }
